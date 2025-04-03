a956415be4b97e5a4b6a53541b1a05009504b2b1
Emmydee614 <emmydee1@Emmydee.emmydee>
2025-04-03 07:17:25 -0400

    this is webdeves class code

---
+++ class-demo/hello.py
@@ -0,0 +1,25 @@
+# this line is running python code without a variable
+print("hello world")
+print ('I am learning python')
+
+
+# Declaring a variable
+first_name = "Emmanuel"
+last_name = "David"
+
+#concatenation
+print(first_name + " " + last_name)
+
+# Types of variables
+# integer whole numbers (eg: 0-9)
+# floating-point (float) - decimal numbers (e.g 0.1)
+# string (str) - text data ("hello", "python",)
+# Boolean (bool) - True/false values
+
+age = 18
+sentence = "The legal age to vote in"
+country = "Nigeria"
+
+full_sentence = sentence + " " + country
+
+print(full_sentence + " is " + str(age))
